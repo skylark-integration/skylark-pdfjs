3454bae9d606febf3ffb98e1ab79c142c236c539
hudaokeji <github@hudaokeji.com>
2021-04-04 22:32:07 +0900

    init commit

---
+++ dist/skylark-pdfjs/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-pdfjs - A version of video.js that ported to running on skylarkjs.
+ * @author Hudaokeji Co.,Ltd
+ * @version v0.9.0
+ * @link www.skylarkjs.org
+ * @license MIT
+ */
+define(["skylark-langx/skylark","./pdf"],function(n,a){return n.attach("intg.pdfjs",a)});
+//# sourceMappingURL=sourcemaps/main.js.map
